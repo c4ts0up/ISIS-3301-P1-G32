58bfdda23efc7190d184213040f08fb56b3d241c
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-04-16 17:56:31 -0500

    Adelantado

--- turismo-alpes/back_aplicacion/pruebaFastAPI.py
+++ turismo-alpes/back_aplicacion/pruebaFastAPI.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 import random
 import time
+import pandas as pd
+import io
 
 app = FastAPI()
 
@@ -20,10 +23,18 @@ class Resena(BaseModel):
 @app.post("/a")
 async def read_root(resena: Resena):
     #Resena a predecir
-    res = resena.res
-    
+    res = resena.res 
     #Falta hacer la prediccion
-    time.sleep(20)
-
+    time.sleep(2)
     #Enviar prediccion
     return {"pred": random.randint(1, 5)}
+
+@app.post("/b")
+async def csvPred(file: UploadFile = File(...)):
+    csv_content = await file.read()
+    df = pd.read_csv(io.BytesIO(csv_content))
+    print(df.head(5))
+    print(df.shape)
+    processed_csv = df.to_csv(index=False)
+    
+    return StreamingResponse(io.BytesIO(processed_csv.encode()), media_type="text/csv", headers={"Content-Disposition": "attachment; filename=processed_file.csv"})
