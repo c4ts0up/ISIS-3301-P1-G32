67d8c94282042c3065957790e426ac0a534b3553
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-04-16 17:56:31 -0500

    Adelantado

--- turismo-alpes/front_aplicacion/src/App.js
+++ turismo-alpes/front_aplicacion/src/App.js
@@ -6,14 +6,22 @@ import { useState } from "react";
 
 function App() {
 
-  const [resena, setResena] = useState("")
-  const[terminado, setTerminado] = useState(false)
+  const [resena, setResena] = useState("");
+  const[terminado, setTerminado] = useState(false);
   const[prediccion, setPrediccion] = useState(0);
+  const[file, setFile] = useState(null);
+  const[termiandoFile, setTerminadoFile] = useState(false);
 
 
   const handleResena = (e) =>{
     const res = e.target.value;
     setResena(res)
+  }
+
+  const handleFileChange = (e)=>{
+
+    setFile(e.target.files[0]);
+
   }
 
   const handlePredecir = async (e)=>{
@@ -46,10 +54,43 @@ function App() {
     }
   }
 
+  const handlePredecir2 = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/b', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = "Predicciones"
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(url);
+      setTerminadoFile(true);
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+
+
   return (
     <div className="container">
       <Row>
-        <h1>SOY UNA APLICACION DE BI</h1>
+        <h1>SOY UNA APLICACION DE BI SOLO UNA RESENA</h1>
       </Row>
       <Row>
         <Form>
@@ -57,7 +98,7 @@ function App() {
             <Form.Label>
               Ingrese la reseña
             </Form.Label>
-            <Form.Control  id="Resena"  as="textarea" rows={5} placeholder='Ingrese su reseña aca'  value={resena} onChange={handleResena}></Form.Control>
+            <Form.Control  id="Resena"  as="textarea" rows={5} placeholder='Ingrese su reseña aca'  value={resena} onChange={handleResena}/>
           </Form.Group>
         </Form>
       </Row>
@@ -71,6 +112,33 @@ function App() {
       <Row>
         {terminado && <h3> La reseña tiene es de la clase: {prediccion}</h3>}
       </Row>
+
+      <Row>
+        <h1>SOY UNA APLICACION DE BI UN ARCHIVO CSV PARA CLASIFICAR RESENAS</h1>
+      </Row>
+
+      <Row>
+        <Form>
+          <Form.Group>
+            <Form.Label>
+              Archivo CSV con resenas a predecir
+            </Form.Label>
+            <Form.Control type="file" onChange={handleFileChange}/>
+          </Form.Group>
+        </Form>
+      </Row>
+
+      <Row>
+        <Button variant="primary" type="submit" onClick={handlePredecir2}>
+          Predecir
+        </Button>
+      </Row>
+
+      <Row>
+        {termiandoFile && <h3> Descargue el archivo</h3>}
+      </Row>
+
+
       
     </div>
   );
